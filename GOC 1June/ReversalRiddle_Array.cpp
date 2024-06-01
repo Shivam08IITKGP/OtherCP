669f5441f084b7cf4a0f8cb8aae863501a120a93
Shivam Choudhury <124419114+Shivam08IITKGP@users.noreply.github.com>
2024-06-01 20:02:41 +0530

    Update ReversalRiddle_Array.cpp

--- GOC 1June/ReversalRiddle_Array.cpp
+++ GOC 1June/ReversalRiddle_Array.cpp
@@ -5,9 +5,14 @@ void solve()
     vector<int> a(n + 1);
     map<int, int> m;
     int ans = 0;
+    // We have to count the number of pairs l,r such that a[l]==a[r],
+    // l<r because reversing the substring l+1, r-1 be will give the same
+    // string as reversing l,r.
     rep(i, 1, n)
     {
         cin >> a[i];
+        // We are adding the subsegments that start from i to the left
+        // i.e. [j,i] where j belongs to [1,i-1]
         ans += i - m[a[i]] - 1;
         m[a[i]]++;
     }
