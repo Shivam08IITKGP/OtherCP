97d127348484e32962eadd8f994f1ed369bf9fed
Shivam Choudhury <124419114+Shivam08IITKGP@users.noreply.github.com>
2024-06-12 22:57:09 +0530

    Create PREFSUFF.cpp

---
+++ Codechef/PREFSUFF.cpp
@@ -0,0 +1,19 @@
+void solve()
+{
+    int n;
+    cin >> n;
+    if(n==1)
+    {
+        cout<<1<<endl;
+        return;
+    }
+
+    for(int i=0;i<=n-3;i++)
+    {
+        cout<<5<<" ";
+    }
+    cout<<"6 3"<<endl;
+    return;
+    
+}
+
