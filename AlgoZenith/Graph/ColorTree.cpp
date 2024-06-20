bd76bb7e94380b754277f0500416245a36b544d0
Shivam Choudhury <124419114+Shivam08IITKGP@users.noreply.github.com>
2024-06-20 11:33:16 +0530

    Create ColorTree.cpp

---
+++ AlgoZenith/Graph/ColorTree.cpp
@@ -0,0 +1,11 @@
+/*Description*
+
+You have been given a tree with N nodes and N - 1 edges. You want to colour each node, 
+such that no two adjacent nodes (directly connected by an edge) and no two 
+nearly-adjacent nodes (both directly connected to a common node with edges) has the same colour. 
+Your task is to find the minimum number of colours required to accomplished this. 
+*/
+
+/*
+If maximum degree is D, then answer is D+1
+*/
