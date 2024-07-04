29e0b8e505be118db43aa03534510c4f0279d03d
Shivam Choudhury <124419114+Shivam08IITKGP@users.noreply.github.com>
2024-07-04 11:51:01 +0530

    Update NimbleGame.cpp

--- HackerRank/GameTheory/NimbleGame.cpp
+++ HackerRank/GameTheory/NimbleGame.cpp
@@ -5,13 +5,13 @@ void solve()
     vi a(n);
     read(a);
     int x = 0;
-    fr(i, n - 1)
+    fr(i, n)
     {
-        if (a[i + 1] % 2 == 0)
-            x ^= (i + 1);
-            //Actually what is happening is x^ = (i+1)-> a[i+1] times xor
-            //So that's why we take xor of only those i+1 which are having
-            //odd a[i+1], (even ones will get canceled out)
+        if (a[i] % 2 != 0)
+            x ^= (i);
+        // Actually what is happening is x^ = (i)-> a[i] times xor
+        // So that's why we take xor of only those i+1 which are having
+        // odd a[i], (even ones will get canceled out)
     }
     if (x > 0)
     {
